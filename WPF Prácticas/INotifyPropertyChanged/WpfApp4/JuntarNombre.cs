59d4413b6c308f42703085229035dd15a33279ff
unknown <facundoromani90@gmail.com>
2022-05-10 11:22:49 -0300

    Curso Wpf

---
+++ WPF Prácticas/INotifyPropertyChanged/WpfApp4/JuntarNombre.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    public class JuntarNombre : INotifyPropertyChanged
+    {
+
+        private string nombre, apellido, nombre_completo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged("Nombre_completo");
+            }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value;
+                // Si tiene cambio la caja de texto se aplica al textBox3 Nombre Completo.
+                OnPropertyChanged("Nombre_completo");    
+            }
+        }
+
+        public string Nombre_completo
+        {
+            get { nombre_completo = Nombre + " " + Apellido;
+                return nombre_completo;
+            }
+            set { }
+        }
+    }
+}
